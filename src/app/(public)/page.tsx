6daf910f73e719ec8165dfba3a61567aa0bb62af
MxAziz <hereabdulaziz@gmail.com>
2025-10-30 18:40:37 +0600

    Using Incremental Static Regeneration for data fetching which is fetch and rebuild data after a specific time

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -4,17 +4,19 @@ import { IPost } from "@/types";
 
 export default async function HomePage() {
 
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/post`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/post`,{next:{revalidate: 30}});
   const {data: blogs} = await res.json();
 
   return (
     <div>
+      {/* hero section */}
       <Hero />
+      {/* featured posts section */}
       <h2 className="text-center my-12 text-5xl">Featured Posts</h2>
-      <div className="grid grid-cols-3 gap-5 px-8 mb-8 mt-16">
-        {
-          blogs.map((blog: IPost) =><BlogCard key={blog.id} post={blog} />)
-        }
+      <div className="grid grid-cols-3 gap-5 px-8 my-16">
+        {blogs.slice(0, 3).map((blog: IPost) => (
+          <BlogCard key={blog.id} post={blog} />
+        ))}
       </div>
     </div>
   );

0ecd61ffbb4529d75227bfc2187e04379be19904
MxAziz <hereabdulaziz@gmail.com>
2025-10-31 02:29:21 +0600

    generate Static Params

--- src/app/(public)/blogs/[id]/page.tsx
+++ src/app/(public)/blogs/[id]/page.tsx
@@ -1,9 +1,16 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import BlogDetailsCard from '@/components/modules/Blogs/BlogDetailsCard';
+import { IPost } from '@/types';
 import React from 'react';
 
-const BlogDetailsPage = async (context : any) => {
-    const { id } = context.params;
+export const generateStaticParams = async () => {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/posts`);
+    const { data: blogs } = await res.json();
+
+    return blogs.slice(0, 3).map((blog: IPost) => ({id: blog.id.toString()}));
+}
+
+const BlogDetailsPage = async ({params}: {params: Promise<{id: string}>}) => {
+    const { id } = await params;
 
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/post/${id}`)
     const blog = await res.json();

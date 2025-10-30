71f28361f71629adc7a1b3fc0a6bcb1f76e097a4
MxAziz <hereabdulaziz@gmail.com>
2025-10-30 20:26:43 +0600

    successfully fetch all blog post data for blogs route and added loading spiner

---
+++ src/app/(public)/blogs/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const loading = () => {
+    return (
+        <div className="flex items-center justify-center">
+            <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-amber-400"></div>
+        </div>
+    );
+};
+
+export default loading;
--- src/app/(public)/blogs/page.tsx
+++ src/app/(public)/blogs/page.tsx
@@ -1,7 +1,17 @@
-const AllBlogsPage = () => {
+import BlogCard from "@/components/modules/Blogs/BlogCard";
+import { IPost } from "@/types";
+
+const AllBlogsPage = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/post`);
+  const { data: blogs } = await res.json();
   return (
     <div className="py-30 px-4 max-w-7xl mx-auto">
       <h2 className="text-center text-4xl">All Blogs</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-8 mt-16">
+        {
+          blogs.map((blog: IPost)=><BlogCard key={blog.id} post={blog} />)
+        }
+      </div>
     </div>
   );
 };

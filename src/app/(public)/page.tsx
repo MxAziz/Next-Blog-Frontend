f42e9a40ece16da101ed5b8a62bf31d7d1fda22e
MxAziz <hereabdulaziz@gmail.com>
2025-11-03 01:32:47 +0600

    text git snipet

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -12,7 +12,7 @@ export default async function HomePage() {
       {/* hero section */}
       <Hero />
       {/* featured posts section */}
-      <h2 className="text-center my-12 text-5xl">Featured Posts</h2>
+      <h2 className="text-center my-12 text-5xl">Featured Posts here</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-8 my-16">
         {blogs.slice(0, 3).map((blog: IPost) => (
           <BlogCard key={blog.id} post={blog} />

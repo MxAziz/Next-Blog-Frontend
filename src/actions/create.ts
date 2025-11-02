eb8177471cc34bd189773628ae2d52a9b08b2e74
MxAziz <hereabdulaziz@gmail.com>
2025-11-03 01:23:30 +0600

    added server action function for create blogs

--- src/actions/create.ts
+++ src/actions/create.ts
@@ -1,3 +1,33 @@
+"use server";
+import { revalidatePath, revalidateTag } from "next/cache";
+import { redirect } from "next/navigation";
+
 export const create = async (data: FormData) => {
-    console.log(data);
-}
+  const blogInfo = Object.fromEntries(data.entries());
+  const modifiedData = {
+    ...blogInfo,
+    tags: blogInfo.tags
+      .toString()
+      .split(",")
+      .map((tag) => tag.trim()),
+    authorId: 1,
+    isFeatured: Boolean(blogInfo.isFeatured),
+  };
+
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/post`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(modifiedData),
+  });
+
+  const result = await res.json();
+
+  if (result?.id) {
+    revalidateTag("BLOGS");
+    revalidatePath("/blogs");
+    redirect("/");
+  }
+  return result;
+};

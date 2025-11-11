c8705492700163a2096d6204cf4811e1b4cfd9e4
MxAziz <hereabdulaziz@gmail.com>
2025-11-11 22:08:40 +0600

    uncomment login and register form and use it

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -3,23 +3,22 @@ import Hero from "@/components/modules/Home/Hero";
 import { IPost } from "@/types";
 
 export default async function HomePage() {
+     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/post`, {
+          next: { tags: ["BLOGS"] },
+     });
+     const { data: blogs } = await res.json();
 
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/post`, {
-    next: {tags: ["BLOGS"] }
-  });
-  const {data: blogs} = await res.json();
-
-  return (
-    <div>
-      {/* hero section */}
-      <Hero />
-      {/* featured posts section */}
-      <h2 className="text-center my-12 text-5xl">Featured Posts</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-8 my-16">
-        {blogs.slice(0, 3).map((blog: IPost) => (
-          <BlogCard key={blog.id} post={blog} />
-        ))}
-      </div>
-    </div>
-  );
+     return (
+          <div>
+               {/* hero section */}
+               <Hero />
+               {/* featured posts section */}
+               <h2 className="text-center my-12 text-5xl">Featured Posts</h2>
+               <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-8 my-16">
+                    {blogs.slice(0, 3).map((blog: IPost) => (
+                         <BlogCard key={blog.id} post={blog} />
+                    ))}
+               </div>
+          </div>
+     );
 }
